778c0f65cde237bef30b5180df4ba3f323d6c8ba
Madan Khanal <46622438+Sayak003@users.noreply.github.com>
2025-09-13 16:19:22 +0545

    Coment added

---
+++ Assignment1.py
@@ -0,0 +1,7 @@
+X=input("Please Enter your Name: ")
+Y=input("Please Enter your Age: ")
+print("Hello " + X + "! You are " + Y + " years old.")
+
+Hobbies=["Reading", "Travelling","Horse Riding"]
+Hobbies.append("Painting")
+print("My Hobbies are: ", Hobbies)
