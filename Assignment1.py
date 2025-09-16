7bf70805272f5a92cba019d7354c89eee3a1b384
Madan Khanal <46622438+Sayak003@users.noreply.github.com>
2025-09-16 20:40:58 +0545

    New line Code

--- Assignment1.py
+++ Assignment1.py
@@ -4,4 +4,10 @@ print("Hello " + X + "! You are " + Y + " years old.")
 
 Hobbies=["Reading", "Travelling","Horse Riding"]
 Hobbies.append("Painting")
-print("My Hobbies are: ", Hobbies)
+print("My Hobbies are: ", Hobbies)#comment
+print("My Hobbies are: ", Hobbies[0], Hobbies[1], Hobbies[2], Hobbies[3])#comment
+if "Reading" in Hobbies:
+    print("I love Reading") #comment
+else:
+    print("I don't like Reading") #comment      
+#comment
